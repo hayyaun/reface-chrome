56293bbf00756a485e4b993b680349a2125ba15c
Hayyaun <hayyaun@outlook.com>
2025-09-29 20:15:43 +0330

    search

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useDeferredValue, useMemo, useState, type ReactNode } from "react";
+import { RiSettingsFill } from "react-icons/ri";
 import PatchList from "../components/PatchList";
 import Settings from "../components/Settings";
 import { useStore } from "../store";
@@ -8,26 +9,49 @@ export default function App() {
   const urls = useStore((s) => s.urls);
   const [selected, setSelected] = useState<string | null>(null);
 
+  const [query, setQuery] = useState("");
+  const deferredQuery = useDeferredValue(query);
+
+  const relevantItems = useMemo(
+    () => Object.keys(urls).filter((k) => k.includes(deferredQuery)),
+    [deferredQuery, urls],
+  );
+
   return (
     <>
       <header className="flex items-center justify-between gap-2 bg-white/5 p-2 text-xs">
         Settings
       </header>
       <main className="relative flex min-h-0 flex-1 items-stretch">
-        <aside className="flex basis-48 flex-col overflow-y-auto border-r border-white/5">
-          <ListItem
-            title="General"
-            active={selected === null}
-            onClick={() => setSelected(null)}
-          />
-          {Object.keys(urls).map((url, i) => (
+        <aside className="flex basis-48 flex-col border-r border-white/5">
+          <div className="flex flex-1 flex-col overflow-y-auto">
             <ListItem
-              key={i}
-              title={url}
-              active={selected === url}
-              onClick={() => setSelected(url)}
+              title={
+                <span className="flex items-center justify-between gap-1">
+                  General
+                  <RiSettingsFill />
+                </span>
+              }
+              active={selected === null}
+              onClick={() => setSelected(null)}
             />
-          ))}
+            {relevantItems.map((url, i) => (
+              <ListItem
+                key={i}
+                title={url}
+                active={selected === url}
+                onClick={() => setSelected(url)}
+              />
+            ))}
+          </div>
+          <div className="p-2">
+            <input
+              className="w-full rounded-full bg-white/5 p-1.5 px-4"
+              placeholder="Search"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
         </aside>
         {!selected ? <Settings /> : <PatchList hostname={selected} />}
       </main>
@@ -40,7 +64,7 @@ function ListItem({
   active,
   onClick,
 }: {
-  title: string;
+  title: ReactNode;
   active: boolean;
   onClick: () => void;
 }) {
