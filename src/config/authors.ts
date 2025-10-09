e45b1547bafeaea24c3eda52d0dd82331aa1e117
Hayyaun <hayyaun@outlook.com>
2025-10-09 20:03:39 +0330

    update authors

--- src/config/authors.ts
+++ src/config/authors.ts
@@ -8,7 +8,12 @@ const authors: Author[] = [
     donation:
       "https://dogechain.info/address/DT5wiVm7pwJsomK1inx2tdnNLSvY5kvL88",
   },
-  { name: "Arman", github: "" },
+  {
+    name: "Arman",
+    github: "armansal1519",
+    donation:
+      "https://etherscan.io/address/0x390131d9993D28B563420C9b275e899FD2d3D67b",
+  },
 ];
 
 export default authors;
