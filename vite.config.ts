cec46fbba94477b7a8c313507656825979cde2a0
Hayyaun <hayyaun@outlook.com>
2025-10-14 09:11:25 +0330

    add compile-time to app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import compileTime from "vite-plugin-compile-time";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [compileTime(), react(), tailwindcss()],
   build: {
     outDir: "dist",
     rollupOptions: {
