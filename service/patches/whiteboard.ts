5aa1bc7d9a5c63e17168be63f5461d1b23b39d05
Hayyaun <hayyaun@outlook.com>
2025-10-24 23:53:39 +0330

    update whiteboard, add type mode

--- service/patches/whiteboard.ts
+++ service/patches/whiteboard.ts
@@ -4,7 +4,7 @@ const scale = (config["scale"] ?? 0.5) as number;
 const canvasSize = [document.body.scrollWidth, document.body.scrollHeight];
 
 interface State {
-  mode: "draw" | "work";
+  mode: "draw" | "type" | "work";
   canvas: HTMLCanvasElement;
   panel: HTMLDivElement;
   modeBtn: HTMLDivElement;
@@ -23,47 +23,95 @@ const state: State = {
   thickness: 5 * scale,
 };
 
-function addDrawListener() {
-  const ctx = state.canvas.getContext("2d")!;
+const getModeText = () => {
+  if (state.mode === "draw") return "Draw mode";
+  if (state.mode === "type") return "Type mode";
+  return "Neutral";
+};
+
+function setMode(mode: State["mode"]) {
+  state.mode = mode;
+  state.canvas.style.pointerEvents = mode === "work" ? "none" : "auto";
+  state.modeBtn.textContent = getModeText();
+}
+
+function updateColor(ctx: CanvasRenderingContext2D, color: string) {
+  state.color = color;
+  // ui
+  state.pickerBtn.style.color = state.color;
+  // ctx
+  ctx.strokeStyle = state.color;
+  ctx.fillStyle = state.color;
+}
+
+function addDrawListeners(ctx: CanvasRenderingContext2D) {
   let drawing = false;
   // Mouse events
-  state.canvas.addEventListener("mousedown", (e) => {
+  state.canvas.addEventListener("mousedown", (ev) => {
+    if (state.mode !== "draw") return;
     drawing = true;
     ctx.beginPath();
-    ctx.strokeStyle = state.color;
-    ctx.lineWidth = state.thickness;
+    ctx.lineWidth = state.thickness; // TODO move
     ctx.lineJoin = "round";
     ctx.lineCap = "round";
-    ctx.moveTo(e.offsetX * scale, e.offsetY * scale);
+    ctx.moveTo(ev.offsetX * scale, ev.offsetY * scale);
   });
-  state.canvas.addEventListener("mousemove", (e) => {
+  state.canvas.addEventListener("mousemove", (ev) => {
     if (!drawing) return;
-    ctx.lineTo(e.offsetX * scale, e.offsetY * scale);
+    ctx.lineTo(ev.offsetX * scale, ev.offsetY * scale);
     ctx.stroke();
   });
   state.canvas.addEventListener("mouseup", () => (drawing = false));
   state.canvas.addEventListener("mouseout", () => (drawing = false));
 }
 
-function toggleMode() {
-  if (state.mode === "draw") {
-    state.mode = "work";
-    state.modeBtn.textContent = "Work mode";
-    state.canvas.style.pointerEvents = "none";
-  } else {
-    state.mode = "draw";
-    state.modeBtn.textContent = "Draw mode";
-    state.canvas.style.pointerEvents = "auto";
+function addTypingListeners(ctx: CanvasRenderingContext2D) {
+  let pos = [0, 0];
+  let text = "";
+  state.canvas.addEventListener("click", (ev) => {
+    setMode("type");
+    pos = [ev.offsetX * scale, ev.offsetY * scale];
+  });
+  function stopPropagation(ev: KeyboardEvent) {
+    if (state.mode !== "type") return;
+    ev.stopImmediatePropagation();
+    ev.stopPropagation();
+    ev.preventDefault();
   }
-}
+  window.addEventListener(
+    "keydown",
+    (ev) => {
+      if (state.mode !== "type") return;
+      stopPropagation(ev);
+      if (ev.key === "Enter") {
+        // Set font style and size
+        ctx.font = `${48 * scale}px Roboto`; // TODO move
+        ctx.fillText(text, pos[0], pos[1]);
+        // reset
+        text = "";
+        setMode("draw");
+        // TODO hide prerendered text
+        return;
+      }
 
-function updateColor(color: string) {
-  state.color = color;
-  state.pickerBtn.style.color = color;
+      // Fill text
+      if (ev.key === "Backspace") {
+        text = text.slice(0, -1);
+      } else if (ev.key.length === 1) {
+        text += ev.key;
+      }
+
+      // TODO show a prerendered text
+    },
+    true,
+  );
+  // Avoid
+  window.addEventListener("keypress", stopPropagation, true);
+  window.addEventListener("keyup", stopPropagation, true);
 }
 
 function init() {
-  // create and insert a canvas into the document
+  // Create canvas
   state.canvas = document.createElement("canvas");
   state.canvas.width = canvasSize[0] * scale;
   state.canvas.height = canvasSize[1] * scale;
@@ -71,21 +119,23 @@ function init() {
   state.canvas.classList.add("reface__whiteboard-canvas");
   document.body.appendChild(state.canvas);
 
-  addDrawListener();
+  const ctx = state.canvas.getContext("2d")!;
+  addDrawListeners(ctx);
+  addTypingListeners(ctx);
 
-  // create panel
+  // Create panel
   state.panel = document.createElement("div");
   state.panel.classList.add("reface__whiteboard-panel");
   document.body.appendChild(state.panel);
 
-  // create panel buttons
-  // Mode
+  // Create panel buttons
+  // -- Mode
   state.modeBtn = document.createElement("div");
   state.modeBtn.classList.add("reface__whiteboard-btn");
-  state.modeBtn.textContent = "Draw mode";
-  state.modeBtn.addEventListener("click", toggleMode);
+  state.modeBtn.textContent = getModeText();
+  state.modeBtn.addEventListener("click", () => setMode(state.mode === "work" ? "draw" : "work"));
   state.panel.appendChild(state.modeBtn);
-  // Color Picker
+  // -- Color Picker
   const colorPicker = document.createElement("input");
   colorPicker.id = "reface__whiteboard-color-picker";
   colorPicker.type = "color";
@@ -93,7 +143,7 @@ function init() {
   colorPicker.value = state.color;
   colorPicker.addEventListener("input", (ev) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    updateColor((ev.target as any).value as string);
+    updateColor(ctx, (ev.target as any).value as string);
   });
   state.pickerBtn = document.createElement("label");
   state.pickerBtn.htmlFor = colorPicker.id;
@@ -104,6 +154,10 @@ function init() {
   const colorDot = document.createElement("div");
   colorDot.classList.add("reface__whiteboard-picker");
   state.pickerBtn.appendChild(colorDot);
+
+  // Initialization
+  setMode("draw");
+  updateColor(ctx, state.color);
 }
 
 init();
